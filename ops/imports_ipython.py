0cc48c6cb2c8ed70f862469a792dc3bb27d1dd94
Sasha <sashakmurray@gmail.com>
2022-07-20 10:18:53 -0400

    Attempted and failed to make example work

--- ops/imports_ipython.py
+++ ops/imports_ipython.py
@@ -10,7 +10,7 @@ IPython.get_ipython().run_line_magic('autoreload', '2')
 
 
 import matplotlib.pyplot as plt
-import seaborn as sns
+# import seaborn as sns
 
 from tqdm.auto import tqdm
-tqdm.pandas() # adds GroupBy.progress_apply
+tqdm.pandas()  # adds GroupBy.progress_apply
